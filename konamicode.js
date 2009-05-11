d5e16ea769a9b1fbfdb87b913037a680f4f18d5b
Kyle Bragger <kyle.bragger@gmail.com>
2009-05-11 19:41:15 -0400

    totally refactored key logging thanks to jacob dehart

--- konamicode.js
+++ konamicode.js
@@ -21,18 +21,21 @@
 (function(jQuery) {
     jQuery.fn.upUpDownDown = function(o){
         var options = jQuery.extend({
-                                        watchFor : '38384040373937396665',
+                                        watchFor : [38,38,40,40,37,39,37,39,66,65],
                                         callback : function() { }
                                     }, o);
         
-        var key_accum = '';
-        var match = options.watchFor || '38384040373937396665';
-        
+        var key_accum = [];
+        var match = options.watchFor;
+
         $(document).keyup(function(e){
-            key_accum = key_accum + (e.keyCode ? e.keyCode : e.charCode);
-            if (key_accum.indexOf(match) != -1)
+            len = key_accum.push(e.keyCode ? e.keyCode : e.charCode);
+            
+            if(len > match.length) key_accum.shift();
+            
+            if (key_accum.join('-') == match.join('-'))
             {
-                key_accum = '';
+                key_accum = [];
                 if (options.callback)
                 {
                     options.callback($(this));
